14a56f1b8b40dcb5ae5fa0e853499388e6aa21c0
Roman Oravec <roman.oravec@invasys.com>
2022-12-06 09:25:26 +0100

    day6 sol 2

--- day6/day6.py
+++ day6/day6.py
@@ -2,11 +2,11 @@
 with open('day6/input.txt', 'r') as f:
     data = f.read()
 
-
-marker = ['0'] * 4
+n = 14
+marker = ['0'] * n
 for i in range(len(data)):
-    marker[i % 4] = data[i]
-    if len(set(marker)) == 4 and ''.join(marker).isalpha():
+    marker[i % n] = data[i]
+    if len(set(marker)) == n and ''.join(marker).isalpha():
         print(set(marker))
         print(i+1)
         break
