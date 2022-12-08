c0f89cebe0d6264c5fe659a71d5b888b94ee0e1a
Roman Oravec <roman.oravec@invasys.com>
2022-12-08 14:09:02 +0100

    very shitty solution of 8.2

--- day8/day8.py
+++ day8/day8.py
@@ -1,5 +1,34 @@
 import numpy as np
 
+
+def score(arr):
+    # print(arr)
+    if len(arr) <= 1:
+        #print("cant see shit")
+        return 1
+    s = 0
+    my_tree = arr[0]
+    for a in arr[1:]:
+        s += 1
+        if a >= my_tree:
+            break
+    #print("I see ", s)
+    return s
+
+
+def total_score(x, y, data):
+    s = 1
+    # print("DOWN")
+    s *= score(data[x:, y])
+    # print("RIGHT")
+    s *= score(data[x][y:])
+    # print("UP")
+    s *= score(np.flip(data[:x+1, y]))
+    # print("LEFT")
+    s *= score(np.flip(data[x][:y+1]))
+    return s
+
+
 with open('day8/input.txt', 'r') as f:
     data = np.array([list(map(int, x)) for x in f.read().splitlines()])
 
@@ -11,4 +40,12 @@ for x, y in np.ndindex(data[:dim, :dim].shape):
             or np.all(e > data[:x, y]) or np.all(e > data[x][:y]):
         res += 1
 
-print(res)
+# print(res)
+
+# 2
+m = 0
+for x, y in np.ndindex(data.shape):
+    s = total_score(x, y, data)
+    m = max(m, s)
+
+print(m)
