8de366a7ef1eb6cbaf9f8e012632f082c207b7dd
Roman Oravec <romanoravec1@gmail.com>
2022-12-10 16:35:03 +0100

    halfassed part2

--- day10/day10.py
+++ day10/day10.py
@@ -1,32 +1,48 @@
-def inc_cycle(cycle, reg, cycles):
+def inc_cycle(cycle, reg):
+  draw(reg, cycle)
   cycle += 1
   res = 0
-  if cycle in cycles:
-    res = cycle*reg
-    #print(f"CYCLE {cycle}",res)
   return cycle, res
 
+def show_sprite(reg, cycle):
+  print(f"CYCLE {cycle}")
+  print("SPRITE POSITION:")
+  for i in range(40):
+    if i % 40 in range(reg - 1, reg + 2):  
+      print('#', end='')  
+    else:
+      print('.', end='')
+  print('\n-------------------------------------')
 
-with open('day10/input.txt', 'r') as f:
+def draw(reg, cycle):
+  #show_sprite(reg, cycle)
+  if (cycle % 40) in range(reg-1, reg+2):
+    print('#', end='')
+  else:
+    print('.', end='')
+
+  if (cycle % 40)  == 0:
+    print()
+
+with open('day10/input2.txt', 'r') as f:
   data = f.read().splitlines()
-
 
 reg = 1
 cycle = 1
 signals = 0
-cycles = [20,60,100,140,180,220]
 
 for inst in data:
   if 'noop' in inst:
-    cycle, res = inc_cycle(cycle, reg, cycles)
+    cycle, res = inc_cycle(cycle, reg)
     signals += res
     continue
   else:
     x = int(inst.split()[1])
-    cycle, res = inc_cycle(cycle, reg, cycles)
+    cycle, res = inc_cycle(cycle, reg)
     signals += res
     reg += x
-    cycle, res = inc_cycle(cycle, reg, cycles)
+    cycle, res = inc_cycle(cycle, reg)
     signals += res
 
-print(signals)
+print(cycle)
+#print('\n',signals)
